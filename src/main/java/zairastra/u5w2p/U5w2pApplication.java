a126a2c9fd280b3af499a71fa145848f132db342
ZairaStra <straticozaira@gmail.com>
2025-07-25 09:49:43 +0200

    Initial commit

---
+++ src/main/java/zairastra/u5w2p/U5w2pApplication.java
@@ -0,0 +1,13 @@
+package zairastra.u5w2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w2pApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w2pApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zairastra/u5w2p/U5w2pApplicationTests.java
@@ -0,0 +1,13 @@
+package zairastra.u5w2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
